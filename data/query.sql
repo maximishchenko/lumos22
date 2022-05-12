3dbc43f77c87aecfa90a86204d3909ed39fec57a
Maxim Ishchenko <maxim.ishchenko@gmail.com>
2022-05-12 10:33:49 +0300

    Single SQL Query

--- data/query.sql
+++ data/query.sql
@@ -1,39 +1,50 @@
 use lumos22;
 
--- Количество имейлов  - все имейлы исключая дубли. Дубли по имейлу(оставляем первую регистрацию, 
--- исключаем все повторные, выводим уникальное количество всех имейлов у которых дата регистрации раньше - колонка с датой - “created_at). 
--- Таблицы которые использовать - podio_hooks
-SELECT count(*) as unique_emails FROM
-(SELECT MIN(created_at) AS created_at, COUNT(email) AS email
-FROM podio_hooks_
-GROUP BY email) AS emails;
 
--- Количество оплат всего - количество оплат (все опталы у который в колонке “status” - “Получен”). 
--- Таблицы которые использовать - podio_payments
-SELECT count(status) as success_payments FROM podio_payments where status='Получен';
-
--- Сумма оплат всего - сумма оплат всего  (все опталы у который в колонке “status” - “Получен” и суммировать по колонке “amount”). 
--- Таблицы которые использовать - podio_payments
-SELECT SUM(amount) sum_payments FROM podio_payments where status='Получен';
-
--- Количество оплат от подписчиков (в эту колонку должны попасть все оплаты у которых  в колонке “status” - “Получен”,
--- которые при этом пересекаются по имейлу - смотреть обе таблицы. 
--- Таблицы которые использовать - podio_payments и podio_hooks
-SELECT COUNT(*) AS subscribers_payments_count FROM
-(
-	SELECT podio_payments.status, podio_hooks_.email 
-	FROM podio_payments 
-	INNER JOIN podio_hooks_ 
-	ON podio_payments.email = podio_hooks_.email 
-	WHERE podio_payments.status = 'Получен'
-) 
-AS select_subscribers_payments_count;
-
--- Сумма оплат от подписчиков (в эту колонку должны попасть все оплаты у которых  в колонке “status” - “Получен”, 
--- которые при этом пересекаются по имейлу - смотреть обе таблицы, суммировать по колонке “amount” из таблицы podio_payments.
--- Таблицы которые использовать - podio_payments и podio_hooks
-SELECT SUM(podio_payments.amount) as sum_payments_subscribers  
-FROM podio_payments 
-INNER JOIN podio_hooks_ 
-ON podio_payments.email = podio_hooks_.email 
-WHERE podio_payments.status = 'Получен';
+SELECT 
+	-- Запрос уникальных email-адресов
+	-- Возможно использовать DISTINCT, приведенное решение использовано, 
+	-- т.к. в ТЗ указано ориентироваться на дату в столбце created_at
+	(
+		SELECT 
+			COUNT(*)
+		FROM
+		(
+			SELECT 
+				MIN(created_at) AS created_at, COUNT(email) AS email
+			FROM
+				podio_hooks_
+			GROUP BY 
+				email
+		) AS emails
+	) AS 'Кол-во email-адресов',
+	-- Запрос количества оплат всего
+	(
+		SELECT 
+			COUNT(amount)
+		FROM 
+			podio_payments 
+		WHERE 
+			status='Получен'
+	) AS 'Кол-во оплат всего',
+	-- Запрос суммы оплат всего
+	(
+		SELECT 
+			SUM(amount)
+		FROM 
+			podio_payments 
+		WHERE 
+			status='Получен'
+	) AS 'Сумма оплат всего',
+	-- Запрос количества оплат от подписчиков
+	COUNT(podio_payments.email) AS 'Кол-во оплат от подписчиков',
+	-- Запрос суммы оплат от подписчиков
+	SUM(podio_payments.amount) AS 'Сумма оплат от подписчиков'
+FROM 
+	podio_payments 
+INNER JOIN 
+	podio_hooks_ 
+ON 
+	podio_payments.email = podio_hooks_.email 
+WHERE 
+	podio_payments.status = 'Получен';
